5aed8657e04a246f04c1eba027cec9fb2eb92991
Ivan de la Rubia <ivandelarubia@gmail.com>
2017-08-05 18:06:40 +0200

    new expense msg

--- webhooks/expense.go
+++ webhooks/expense.go
@@ -21,7 +21,7 @@ func HandleExpense(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("HOW MUCH ---->", howMuch)
 	fmt.Println("WHO ---->", who)
 
-	msg := fmt.Sprintf("Perfect! I have recorded that %s owes %s to %s :)", who, howMuch, lastSpeaker)
+	msg := fmt.Sprintf("Perfect! I have registered that %s owes %s euros to %s :)", who, howMuch, lastSpeaker)
 
 	w.Write([]byte(`{"text":"` + msg + `"}`))
 	//fmt.Println(string(b))
