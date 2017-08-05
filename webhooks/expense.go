383496aa1f9837fc7080b16b50b60db4f9831410
Ivan de la Rubia <ivandelarubia@gmail.com>
2017-08-05 16:25:05 +0200

    print expense webhook

--- webhooks/expense.go
+++ webhooks/expense.go
@@ -8,6 +8,8 @@ import (
 )
 
 func HandleExpense(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Handle webhook")
+
 	var body interface{}
 	b, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(b, &body)
